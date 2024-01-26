d3ae4c20670c84cf191b0c7afb9dab6b277e0b0a
Maximiliano Gomez <alexander34223@gmail.com>
2024-01-26 02:02:27 -0300

    ejercicio 6 terminado , agregue sweetalert,animate css y tynicolor

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,39 +1,98 @@
-import "bootstrap/dist/css/bootstrap.css";
-import { Button } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
-import ListaColores from "./ListaColores";
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import ListaColores from './ListaColores';
+import 'animate.css/animate.min.css';
+import tinycolor from 'tinycolor2';
 
 const Formulario = () => {
+  const [inputColor, setInpuntColor] = useState('');
+  const [colorsList, setColorsList] = useState([]);
 
-const [inputColor,setInputColor]=useState('');
-const [arrayColores,setArrayColores]=useState([]);
+  useEffect(() => {
+    const guardarColor = JSON.parse(localStorage.getItem('colors'));
+    if (guardarColor) {
+      setColorsList(guardarColor);
+    }
+  }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const isColorValid = tinycolor(inputColor).isValid();
+
+    if (!inputColor.trim() || !isColorValid) {
+      
+      Swal.fire({
+        icon: 'error',
+        title: '¡Error!',
+        text: 'Por favor, ingrese un color válido.',
+        showClass: {
+          popup: 'animate__animated animate__shakeX',
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOut',
+        },
+        timer: 2000,
+        showConfirmButton: false,
+      });
+      return;
+    }
+
+    const nuevoColor = {
+      id: Date.now(),
+      nombre: inputColor,
+      value: inputColor,
+    };
+    const colorActualizado = [...colorsList, nuevoColor];
+    setColorsList(colorActualizado);
+    localStorage.setItem('colors', JSON.stringify(colorActualizado));
+    setInpuntColor('');
+
+    // Mostrar SweetAlert de éxito con animación
+    Swal.fire({
+      icon: 'success',
+      title: '¡Éxito!',
+      text: 'Se ha agregado un nuevo color.',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown',
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp',
+      },
+      timer: 2000, // 3 segundos
+      showConfirmButton: false, // No mostrar botón "OK"
+    });
+  }
+
+  const handleEliminarColor = (id) => {
+    const colorEliminado = colorsList.filter(inputColor => inputColor.id !== id);
+    setColorsList(colorEliminado);
+    localStorage.setItem('colors', JSON.stringify(colorEliminado));
+  }
 
   return (
-    <section>
-      <Form>
-        <Form.Group
-          className="mb-3 mt-4 d-flex"
-          controlId="exampleForm.ControlInput1"
-        >
-          <Form.Control
-            type="text"
-            className="fw-bold"
-            placeholder="Ingrese su Color!"
-            pattern="^.{5,30}$"
-            title="Ingrese un minimo de 5 caracteres y un maximo de 30"
-            onChange={(e)=> setInputColor(e.target.value)}
-            value={inputColor}
-
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className='mt-4 d-flex mb-4' controlId="example.input">
+          <Form.Control 
+            className='fw-bold'
+            type="text" 
+            placeholder="Ingrese un Color!" 
+            pattern="^[^\d]{3,20}$"
+            title="Debe ingresar un mínimo de 3 y un máximo de 20 caracteres, sin incluir números."
+            onChange={(e) => setInpuntColor(e.target.value)} 
+            value={inputColor} 
           />
-          <Button type="submit">Agregar</Button>
+          <Button  variant="primary" type="submit">
+            Agregar
+          </Button>
         </Form.Group>
       </Form>
 
-      <ListaColores></ListaColores>
-    </section>
+      <ListaColores colorsList={colorsList} handleEliminarColor={handleEliminarColor} />
+    </div>
   );
-};
+}
 
 export default Formulario;
--- src/components/ListaColores.jsx
+++ src/components/ListaColores.jsx
@@ -1,13 +1,35 @@
+import { ListGroup, Button, Card } from "react-bootstrap";
 
-import ListGroup from 'react-bootstrap/ListGroup';
-import ItemColores from './ItemColores';
-
-const ListaColores=()=> {
+const ListaColores = ({ colorsList, handleEliminarColor }) => {
   return (
-    <ListGroup>
-        <ItemColores></ItemColores>
-    </ListGroup>
+    <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", gridGap: "20px" }}>
+      {colorsList.map(color => (
+        <Card key={color.id} className="border-primary">
+          <Card.Body className="Color2 text-light  ">
+            <Card.Title className="text-center fw-bold">{color.nombre}</Card.Title>
+            <div
+              style={{
+                width: "100px",
+                height: "100px",
+                backgroundColor: color.value,
+                margin: "auto",
+                marginBottom: "10px"
+              }}
+            ></div>
+            <div className="text-center"> {/* Nuevo contenedor para centrar el botón */}
+              <Button 
+                variant="danger"
+                type="submit"
+                onClick={() => handleEliminarColor(color.id)}
+              >
+                Eliminar
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
   );
-}
+};
 
 export default ListaColores;
